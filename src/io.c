fd4d327f5f8a8261f9248a576d7a358f26b36cc5
ClarenceYk <myk694204660@gmail.com>
2019-10-22 19:00:58 +0800

    Update src/io.c

---
+++ src/io.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+int spi_write_2bytes(const uint16_t addr, const uint16_t data)
+{
+    return 0;
+}
+
+int spi_read_2bytes(const uint16_t addr, const uint16_t *data)
+{
+    return 0;
+}
