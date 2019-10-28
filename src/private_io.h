7361763615b9ba57f9e2ca570ffca89364d9c6e9
ClarenceYk <myk694204660@gmail.com>
2019-10-28 16:46:13 +0800

    Add private interface: spi_io_operate

--- src/private_io.h
+++ src/private_io.h
@@ -6,10 +6,20 @@
 
 #define AC483_AVAIL_MEM_STA 0x1000
 #define AC483_AVAIL_MEM_END 0x17FF
+#define AC483_OP_READ  0x05
+#define AC483_OP_WRITE 0x01
 
 typedef struct AC483_Ctrl_Msg {
     uint8_t ctrl_h;
     uint8_t ctrl_l;
 } ac483_ctrl_msg_t;
+
+/**
+ * Function: spi_io_operate
+ * ------------------------
+ * 封装 SPI 设备操作
+ *
+ */
+int spi_io_operate(uint8_t rw, uint16_t addr, uint8_t *blck, size_t size);
 
 #endif
